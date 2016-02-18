288f46e6dc49e583769283b789084af9a3c64762
Eric Guo <eric.guo@sandisk.com>
2016-02-18 21:14:27 +0800

    Fix the only testing case.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test 'should get index' do
+    user = User.first
+    sign_in user
     get :index
     assert_response :success
   end
-
 end

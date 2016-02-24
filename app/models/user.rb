2d68799abe9e8d8b759a02cb9e74a991f75aae8b
goofansu <goofan.su@gmail.com>
2016-02-24 13:57:15 +0800

    add validation to redirect to signup after oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,39 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
+  # add validations
+  before_validation :set_password
+  validates :birthday, presence: true
+
+  # if no validation, uncomment following line
+  # before_save :set_password
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.password = Devise.friendly_token[0, 20]
       user.nickname = auth.info.nickname
       user.sex = auth.info.sex
       user.avatar = auth.info.headimgurl
     end
   end
 
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session['devise.wechat_data']
+        user.provider = data['provider']
+        user.uid = data['uid']
+        user.nickname = data['info']['nickname']
+        user.sex = data['info']['sex']
+        user.avatar = data['info']['headimgurl']
+      end
+    end
+  end
+
   def email_required?
     false
+  end
+
+  private
+  def set_password
+    self.password = Devise.friendly_token[0, 20]
   end
 end

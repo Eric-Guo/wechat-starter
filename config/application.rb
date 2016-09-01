eddad76f61c1e40f2f7622d6270aeecae6aac4ca
goofansu <goofan.su@gmail.com>
2016-09-01 18:03:12 +0800

    modify path

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module WechatStarter
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << "#{Rails.root}/lib"
   end
 end

c25c833483ed65ee7940e74c44805ecc3dfff343
goofansu <goofan.su@gmail.com>
2016-09-01 16:28:46 +0800

    fix not logined

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   # to allow using wechat, wechat_oauth2 in controller and wechat_config_js in view
   wechat_api
 

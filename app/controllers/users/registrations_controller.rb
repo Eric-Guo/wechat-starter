c41bdbbec3fbe6e00089b321be98fa75c9124cd1
Eric-Guo <guochunzhong@bayekeji.com>
2017-03-16 10:56:23 +0800

    DEPRECATION WARNING: before_filter is deprecated and will be removed in Rails 5.1. Use before_action instead

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_sign_up_params, only: [:create]
-  # before_filter :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
 
   wechat_api
 

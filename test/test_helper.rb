1fbe86239c8fbabc4d1e6bc15457821120d4a403
YeJun Su <goofan.su@gmail.com>
2017-07-14 19:04:48 +0800

    fix missing devise

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,9 @@
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  include Devise::Test::ControllerHelpers
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 

94b74b2b3b8b7c4421913c10847703ec386519cc
Eric-Guo <eric.guocz@gmail.com>
2023-08-16 21:50:21 +0800

    also need get?

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'wx_pay' => 'pay#wx_pay'
   post 'wx_notify' => 'pay#wx_notify'
+  get 'wx_notify' => 'pay#wx_notify'
   root to: 'home#index'
   resource :wechat, only: [:show, :create]
   devise_for :users, controllers: {

cc5b63f5fb9fbc7ff7392b6dde40dda645c9d7ba
Eric-Guo <eric.guocz@gmail.com>
2023-08-17 00:04:19 +0800

    After do rails app:update

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module WechatStarter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
